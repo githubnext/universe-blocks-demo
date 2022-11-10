d101e0f25342e14edb4d21a7d603a08d23c913fe
Amelia Wattenberger <wattenberger@github.com>
2022-11-10 11:50:51 -0800

    Create basic.machine.js

---
+++ src/basic.machine.js
@@ -0,0 +1,15 @@
+ {
+  id: 'machine',
+  initial: 'inactive',
+  context: {
+    count: 0
+  },
+  states: {
+    inactive: {
+      on: { TOGGLE: 'active' }
+    },
+    active: {
+      on: { TOGGLE: 'inactive' }
+    }
+  }
+}
