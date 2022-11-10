7aa4f769a85a8ecda2231f1472b32c41d5703674
Amelia Wattenberger <wattenberger@github.com>
2022-11-10 13:53:07 -0800

    update state machines

---
+++ src/state-machines/calculator.machine.js
@@ -0,0 +1,274 @@
+import { createMachine } from 'xstate';
+
+export const toggleMachine = createMachine({
+    id: 'calcMachine',
+    context: {
+      display: '0.',
+      operand1: undefined,
+      operand2: undefined,
+      operator: undefined,
+    },
+    strict: true,
+    initial: 'start',
+    on: {
+      CLEAR_EVERYTHING: {
+        target: '.start',
+        actions: ['reset'],
+      },
+    },
+    states: {
+      start: {
+        on: {
+          NUMBER: [
+            {
+              cond: 'isZero',
+              target: 'operand1.zero',
+              actions: ['defaultReadout'],
+            },
+            {
+              cond: 'isNotZero',
+              target: 'operand1.before_decimal_point',
+              actions: ['setReadoutNum'],
+            },
+          ],
+          OPERATOR: {
+            cond: 'isMinus',
+            target: 'negative_number',
+            actions: ['startNegativeNumber'],
+          },
+          DECIMAL_POINT: {
+            target: 'operand1.after_decimal_point',
+            actions: ['defaultReadout'],
+          },
+        },
+      },
+      operand1: {
+        on: {
+          OPERATOR: {
+            target: 'operator_entered',
+            actions: ['recordOperator'],
+          },
+          PERCENTAGE: {
+            target: 'result',
+            actions: ['storeResultAsOperand2', 'computePercentage'],
+          },
+          CLEAR_ENTRY: {
+            target: 'operand1',
+            actions: ['defaultReadout'],
+          },
+        },
+        initial: 'zero',
+        states: {
+          zero: {
+            on: {
+              NUMBER: {
+                target: 'before_decimal_point',
+                actions: 'setReadoutNum',
+              },
+              DECIMAL_POINT: 'after_decimal_point',
+            },
+          },
+          before_decimal_point: {
+            on: {
+              NUMBER: {
+                target: 'before_decimal_point',
+                actions: ['appendNumBeforeDecimal'],
+              },
+              DECIMAL_POINT: 'after_decimal_point',
+            },
+          },
+          after_decimal_point: {
+            on: {
+              NUMBER: {
+                target: 'after_decimal_point',
+                actions: ['appendNumAfterDecimal'],
+              },
+            },
+          },
+        },
+      },
+      negative_number: {
+        on: {
+          NUMBER: [
+            {
+              cond: 'isZero',
+              target: 'operand1.zero',
+              actions: ['defaultNegativeReadout'],
+            },
+            {
+              cond: 'isNotZero',
+              target: 'operand1.before_decimal_point',
+              actions: ['setNegativeReadoutNum'],
+            },
+          ],
+          DECIMAL_POINT: {
+            target: 'operand1.after_decimal_point',
+            actions: ['defaultNegativeReadout'],
+          },
+          CLEAR_ENTRY: {
+            target: 'start',
+            actions: ['defaultReadout'],
+          },
+        },
+      },
+      operator_entered: {
+        on: {
+          OPERATOR: [
+            {
+              cond: 'isNotMinus',
+              target: 'operator_entered',
+              actions: 'setOperator',
+            },
+            {
+              cond: 'isMinus',
+              target: 'negative_number_2',
+              actions: ['startNegativeNumber'],
+            },
+          ],
+          NUMBER: [
+            {
+              cond: 'isZero',
+              target: 'operand2.zero',
+              actions: ['defaultReadout', 'saveOperand2'],
+            },
+            {
+              cond: 'isNotZero',
+              target: 'operand2.before_decimal_point',
+              actions: ['setReadoutNum', 'saveOperand2'],
+            },
+          ],
+          DECIMAL_POINT: {
+            target: 'operand2.after_decimal_point',
+            actions: ['defaultReadout'],
+          },
+        },
+      },
+      operand2: {
+        on: {
+          OPERATOR: [
+            {
+              cond: 'notDivideByZero',
+              target: 'operator_entered',
+              actions: [
+                'storeResultAsOperand2',
+                'compute',
+                'storeResultAsOperand1',
+                'setOperator',
+              ],
+            },
+            {
+              target: 'alert',
+            },
+          ],
+          EQUALS: [
+            {
+              cond: 'notDivideByZero',
+              target: 'result',
+              actions: ['storeResultAsOperand2', 'compute'],
+            },
+            {
+              target: 'alert',
+            },
+          ],
+          CLEAR_ENTRY: {
+            target: 'operand2.zero',
+            actions: ['defaultReadout'],
+          },
+        },
+        initial: 'zero',
+        states: {
+          zero: {
+            on: {
+              NUMBER: {
+                target: 'before_decimal_point',
+                actions: ['setReadoutNum'],
+              },
+              DECIMAL_POINT: 'after_decimal_point',
+            },
+          },
+          before_decimal_point: {
+            on: {
+              NUMBER: {
+                target: 'before_decimal_point',
+                actions: ['appendNumBeforeDecimal'],
+              },
+              DECIMAL_POINT: 'after_decimal_point',
+            },
+          },
+          after_decimal_point: {
+            on: {
+              NUMBER: {
+                target: 'after_decimal_point',
+                actions: 'appendNumAfterDecimal',
+              },
+            },
+          },
+        },
+      },
+      negative_number_2: {
+        on: {
+          NUMBER: [
+            {
+              cond: 'isZero',
+              target: 'operand2.zero',
+              actions: ['defaultNegativeReadout'],
+            },
+            {
+              cond: 'isNotZero',
+              target: 'operand2.before_decimal_point',
+              actions: ['setNegativeReadoutNum'],
+            },
+          ],
+          DECIMAL_POINT: {
+            target: 'operand2.after_decimal_point',
+            actions: ['defaultNegativeReadout'],
+          },
+          CLEAR_ENTRY: {
+            target: 'operator_entered',
+            actions: ['defaultReadout'],
+          },
+        },
+      },
+      result: {
+        on: {
+          NUMBER: [
+            {
+              cond: 'isZero',
+              target: 'operand1',
+              actions: ['defaultReadout'],
+            },
+            {
+              cond: 'isNotZero',
+              target: 'operand1.before_decimal_point',
+              actions: ['setReadoutNum'],
+            },
+          ],
+          PERCENTAGE: {
+            target: 'result',
+            actions: ['storeResultAsOperand2', 'computePercentage'],
+          },
+          OPERATOR: {
+            target: 'operator_entered',
+            actions: ['storeResultAsOperand1', 'recordOperator'],
+          },
+          CLEAR_ENTRY: {
+            target: 'start',
+            actions: ['defaultReadout'],
+          },
+        },
+      },
+      alert: {
+        invoke: {
+          src: (context, event) => () => {
+            // eslint-disable-next-line no-alert
+            alert('Cannot divide by zero!');
+            return Promise.resolve();
+          },
+          onDone: {
+            target: 'start',
+            actions: ['reset'],
+          },
+        },
+      },
+    },
+});
